9ee3483b7b6b1c0e3a25c74ae4fb854ad283cab4
Drielixxiii <drieli.freitas286@gmail.com>
2023-02-05 15:12:56 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ function ativarLink(link) {
 links.forEach(ativarLink)
 
 //AUTO CHECAGEM DA PÁGINA DE ORÇAMENTOS
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+ const elemento = document.getElementById(parametro)
+ if (elemento) {
+  elemento.checked = true
+
+ }
+ console.log(elemento)
+}
+
+parametros.forEach(ativarProduto)
+
