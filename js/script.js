0683430971f77daeef2e40a4ebbbd4daf7d7683a
Drielixxiii <drieli.freitas286@gmail.com>
2023-02-06 16:37:26 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -25,3 +25,45 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto)
 
+//PERGUNTAS FREQUENTES
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event) {
+ const pergunta = event.currentTarget;
+ const controls = pergunta.getAttribute('aria-controls')
+ const resposta = document.getElementById(controls)
+
+ resposta.classList.toggle('ativa')
+ const ativa = resposta.classList.contains('ativa')
+ console.log(ativa)
+ pergunta.setAttribute('aria-expanded', 'true')
+}
+
+function eventosPerguntas(pergunta) {
+ pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
+
+//GALERIA DAS BICICLETAS
+const galeria = document.querySelectorAll('.bicicleta-imgs img')
+const galeriaContainer = document.querySelector('.bicicleta-imgs')
+
+function trocarImagem(event) {
+ const img = event.currentTarget
+ const media = matchMedia('(min-width:935px)').matches
+ if (media) {
+  galeriaContainer.prepend(img)
+ }
+}
+
+function eventosGaleria(img) {
+ img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+//PLUGIN - Animação
+if (window.SimpleAnime) {
+ new SimpleAnime();
+}
